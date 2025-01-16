33eb38d7714fa4ee2f35c17a2cf6c399dc91b536
khanhlc04 <laothucatkhanhle@gmail.com>
2025-01-17 00:02:42 +0700

    Update: Update env

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -3,15 +3,16 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmer_sV9Va1lflhR1QvBgIMhkQegToYCg",
-  authDomain: "music-app-f2446.firebaseapp.com",
-  databaseURL: "https://music-app-f2446-default-rtdb.firebaseio.com",
-  projectId: "music-app-f2446",
-  storageBucket: "music-app-f2446.firebasestorage.app",
-  messagingSenderId: "583782194040",
-  appId: "1:583782194040:web:026fd7857d2ad874f3468d",
-  measurementId: "G-QXEHYS5305"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
